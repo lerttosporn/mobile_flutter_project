7cff0617689d8167ef10864be70bf41fcd7003c8
lerttosporn <lerttosporn.s@ku.th>
2025-09-26 16:10:01 +0700

    SUM

--- myproject/lib/utils/constant.dart
+++
@@ -1,9 +0,0 @@
-//API URL
-//Localhost
-
-const baseURLAPI = "http://localhost:8800/api";
-const baseURLImage = "http://localhost:8800/uploads/images";
-
-//Server
-//const baseURL = "https://api.example.com/api";  
-//const baseURLImage = "https://api.example.com/upload/images/ ";
