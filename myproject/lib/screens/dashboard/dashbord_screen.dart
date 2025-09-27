432b77fb1eab4b4b78c49ad331b213821db15f88
lerttosporn <78530694+lerttosporn@users.noreply.github.com>
2025-09-27 15:03:20 +0700

    sum

--- myproject/lib/screens/dashboard/dashbord_screen.dart
+++ myproject/lib/screens/dashboard/dashbord_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myproject/themes/colors.dart';
 
 class DashbordScreen extends StatelessWidget {
   const DashbordScreen({super.key});
@@ -7,7 +8,27 @@ class DashbordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Dashboard')),
-      drawer: Drawer(),
+      // menu bar left side
+      drawer: Drawer(
+        backgroundColor: primaryLight,
+        child: Column(
+          children: [
+            ListView(
+              // Important: Remove any padding from the ListView.
+              shrinkWrap: true,
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text("Name"),
+                  accountEmail: Text("Email"),
+                  currentAccountPicture: CircleAvatar(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      // menu bar right side
+      // endDrawer: Drawer(),
       body: const Center(
         child: Text(
           'Welcome to the Dashboard!',
