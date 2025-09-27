ab08b2ee39209fc5638c86446f885b6d7f0010c2
lerttosporn <78530694+lerttosporn@users.noreply.github.com>
2025-09-27 14:16:22 +0700

    update

--- myproject/lib/utils/app_router.dart
+++ myproject/lib/utils/app_router.dart
@@ -1,4 +1,7 @@
 import 'package:myproject/screens/dashboard/dashbord_screen.dart';
+import 'package:myproject/screens/drawerpage/about_screen.dart';
+import 'package:myproject/screens/drawerpage/contact_Screen.dart';
+import 'package:myproject/screens/drawerpage/info_screen.dart';
 import 'package:myproject/screens/login/login_screen.dart';
 import 'package:myproject/screens/register/register_screen.dart';
 import 'package:myproject/screens/welcome/welcome_screen.dart';
@@ -9,10 +12,17 @@ class AppRouter {
   static const String login = 'login';
   static const String register = 'register';
   static const String dashboard = 'dashboard';
+  static const String info = 'info';
+  static const String about = 'about';
+  static const String contact = 'contact';
+
   static get routes => {
     welcome: (context) => const WelcomeScreen(),
     login: (context) => const LoginScreen(),
     register: (context) => const RegisterScreen(),
     dashboard:(context)=>const DashbordScreen(),
+    info: (context) => const InfoScreen(),
+    about: (context) => const AboutScreen(),
+    contact: (context) => const ContactScreen(),
   };
 }
