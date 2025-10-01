d97c910c7bfdb4ed9c85aa81162d21b06798cc6f
lerttosporn <lerttosporn.s@ku.th>
2025-10-01 16:37:11 +0700

    Update product_add.dart

--- myproject/lib/screens/products/product_add.dart
+++ myproject/lib/screens/products/product_add.dart
@@ -11,7 +11,15 @@ class _ProductAddState extends State<ProductAdd> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Add New Product!!! ")),
+      appBar: AppBar(
+        title: const Text("Add New Product!!! "),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: const Icon(Icons.save_alt_outlined),
+          ),
+        ],
+      ),
       body: const Center(child: Text("Add New Product!!! ")),
     );
   }
