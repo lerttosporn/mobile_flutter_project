08e0f0cc3982a2e8c8e7aa9c2012e22eaa6b43c0
lerttosporn <lerttosporn.s@ku.th>
2025-10-02 12:58:57 +0700

    Create product_form.dart

---
+++ myproject/lib/screens/products/components/product_form.dart
@@ -0,0 +1,179 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:myproject/components/custom_textfield.dart';
+import 'package:myproject/models/product_model.dart';
+
+class ProductForm extends StatefulWidget {
+  final ProductModel product;
+  final Function(File? file) callBackSetImage;
+  final GlobalKey<FormState> formKey;
+
+  const ProductForm(
+    this.product, {
+    required this.callBackSetImage,
+    required this.formKey,
+    super.key,
+  });
+
+  @override
+  State<ProductForm> createState() => _ProductFormState();
+}
+
+class _ProductFormState extends State<ProductForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: widget.formKey,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16.0, left: 8.0, right: 8.0),
+        child: Column(
+          children: [
+            customTextFieldProduct(
+              initialValue: widget.product.name.toString(),
+              hintText: 'ชื่อสินค้า',
+              prefixIcon: const Icon(Icons.shopping_bag_outlined),
+              obscureText: false,
+              validator: (value) => (value == null || value.isEmpty)
+                  ? 'กรุณากรอก ชื่อสินค้า'
+                  : null,
+
+              onSaved: (value) => widget.product.description = value!,
+            ),
+            const SizedBox(height: 10),
+            customTextFieldProduct(
+              initialValue: widget.product.description.toString(),
+              hintText: 'รายละเดียด',
+              prefixIcon: const Icon(Icons.shopping_bag_outlined),
+              obscureText: false,
+              validator: (value) => (value == null || value.isEmpty)
+                  ? 'กรุณากรอก รายละเดียดสินค้า'
+                  : null,
+
+              onSaved: (value) => widget.product.description = value!,
+            ),
+            const SizedBox(height: 10),
+            customTextFieldProduct(
+              initialValue: widget.product.barcode.toString(),
+              hintText: 'บาร์โค้ดสินค้า',
+              prefixIcon: const Icon(Icons.shopping_bag_outlined),
+              obscureText: false,
+              validator: (value) => (value == null || value.isEmpty)
+                  ? 'กรุณากรอก บาร์โค้ดสินค้า'
+                  : null,
+
+              onSaved: (value) => widget.product.barcode = value!,
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                customTextFieldProduct(
+                  initialValue: widget.product.price.toString(),
+                  hintText: 'ราคาสินค้า',
+                  prefixIcon: const Icon(Icons.shopping_bag_outlined),
+                  obscureText: false,
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'กรุณากรอก ราคาสินค้า'
+                      : null,
+
+                  onSaved: (value) => widget.product.price = int.parse(value!),
+                ),
+                const SizedBox(height: 10),
+                customTextFieldProduct(
+                  initialValue: widget.product.stock.toString(),
+                  hintText: 'จำนวนสินค้า',
+                  prefixIcon: const Icon(Icons.shopping_bag_outlined),
+                  obscureText: false,
+                  validator: (value) => (value == null || value.isEmpty)
+                      ? 'กรุณากรอก จำนวนสินค้า'
+                      : null,
+
+                  onSaved: (value) => widget.product.stock = int.parse(value!),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            customTextFieldProduct(
+              initialValue: widget.product.name.toString(),
+              hintText: 'ชื่อสินค้า',
+              prefixIcon: const Icon(Icons.shopping_bag_outlined),
+              obscureText: false,
+              validator: (value) => (value == null || value.isEmpty)
+                  ? 'กรุณากรอก บาร์โค้ดสินค้า'
+                  : null,
+
+              onSaved: (value) => widget.product.name = value!,
+            ),
+            const SizedBox(height: 10),
+            customTextFieldProduct(
+              initialValue: widget.product.name.toString(),
+              hintText: 'ชื่อสินค้า',
+              prefixIcon: const Icon(Icons.shopping_bag_outlined),
+              obscureText: false,
+              validator: (value) => (value == null || value.isEmpty)
+                  ? 'กรุณากรอก บาร์โค้ดสินค้า'
+                  : null,
+
+              onSaved: (value) => widget.product.name = value!,
+            ),
+            const SizedBox(height: 10),
+            customTextFieldProduct(
+              initialValue: widget.product.name.toString(),
+              hintText: 'ชื่อสินค้า',
+              prefixIcon: const Icon(Icons.shopping_bag_outlined),
+              obscureText: false,
+              validator: (value) => (value == null || value.isEmpty)
+                  ? 'กรุณากรอก บาร์โค้ดสินค้า'
+                  : null,
+
+              onSaved: (value) => widget.product.name = value!,
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: customTextFieldProduct(
+                    initialValue: widget.product.userId.toString(),
+                    hintText: 'ผู้ใช้',
+                    prefixIcon: const Icon(Icons.person_outline),
+                    obscureText: false,
+                    validator: (value) => value == null || value.isEmpty
+                        ? 'กรุณากรอกผู้ใช้งาน'
+                        : null,
+                    onSaved: (value) =>
+                        widget.product.userId = int.parse(value!),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Expanded(
+                  child: customTextFieldProduct(
+                    initialValue: widget.product.statusId.toString(),
+                    obscureText: false,
+                    hintText: 'สถานะ',
+                    textInputType: TextInputType.number,
+                    prefixIcon: const Icon(Icons.check_circle_outline),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'กรุณากรอกสถานะสินค้า';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) =>
+                        widget.product.statusId = int.parse(value!),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+                height: 10,
+              ),
+              ProductImage(
+                widget.callBackSetImage,
+                image: widget.product.image,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
