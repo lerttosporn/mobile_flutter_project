b8555e44a2cf6ddc9904cfbf56d6f0a794e4fce1
lerttosporn <lerttosporn.s@ku.th>
2025-10-14 16:12:40 +0700

    Create counter_stateful.screen.dart

---
+++ myproject/lib/screens/counter/counter_stateful.screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CounterStatefulScreen extends StatefulWidget {
+  const CounterStatefulScreen({ super.key });
+
+  @override
+  _CounterStatefulScreenState createState() => _CounterStatefulScreenState();
+}
+
+class _CounterStatefulScreenState extends State<CounterStatefulScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
